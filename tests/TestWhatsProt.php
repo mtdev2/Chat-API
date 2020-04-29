34686ad48f63c8f870d36fd5224b5b3e4a95414c
Manuel Zierenner <zierennermanuel@icloud.com>
2020-04-29 05:49:55 +0200

    files changed

--- tests/TestWhatsProt.php
+++ tests/TestWhatsProt.php
@@ -1,14 +1,14 @@
-<?php
+    <?php
+        
+        require_once '../src/whatsprot.class.php';
+        
 
-require_once '../src/whatsprot.class.php';
+          Used to expose protected methods to the testing framework
 
-/**
- * Used to expose protected methods to the testing framework.
- */
-class TestWhatsProt extends WhatsProt
-{
-    public function processInboundDataNode(ProtocolNode $node)
-    {
-        parent::processInboundDataNode($node);
-    }
-}
+        class TestWhatsProt extends WhatsProt
+
+        public function processInboundDataNode(ProtocolNode $node)
+
+            parent::processInboundDataNode($node);
+
+
